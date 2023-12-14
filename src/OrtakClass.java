d8ce19936d0eaec0a7ddc42dbfc806dbe9c3e042
Burhan PEKTAS <pektasburhan39@gmail.com>
2023-12-14 14:41:12 +0100

    burhan v12

--- src/OrtakClass.java
+++ src/OrtakClass.java
@@ -1,5 +1,6 @@
 public class OrtakClass {
     public static void main(String[] args) {
         System.out.println("Buradan bir babayigit geçti...");
+        System.out.println("calisma alani");
     }
 }
