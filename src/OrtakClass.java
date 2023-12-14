3c7ef3978a8d2975e622994bbb85873c804d0a4d
babayigit <babayigit@gmail.com>
2023-12-14 11:39:42 +0300

    first commit

---
+++ src/OrtakClass.java
@@ -0,0 +1,5 @@
+public class OrtakClass {
+    public static void main(String[] args) {
+        System.out.println("Buradan bir babayigit geçti...");
+    }
+}
---
+++ src/babayigit.java
@@ -0,0 +1,13 @@
+public class babayigit {
+    public static void main(String[] args) {
+        System.out.println("Babayigit'in Yeri");
+        /*
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/muratbabayigit/GithubTeamWork_T127.git
+git push -u origin main
+         */
+    }
+}
